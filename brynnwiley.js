46b994b90b34b4649fae603ce4da7a86ec1ac5a7
brynnwiley <93408001+brynnwiley@users.noreply.github.com>
2022-10-31 21:24:38 +0700

    Update brynnwiley.js

--- brynnwiley.js
+++ brynnwiley.js
@@ -1,39 +1,39 @@
-const puppeteer = require('puppeteer');
-const readlineSync = require('readline-sync');
-const randomUseragent = require('random-useragent');
-
-
-var uname = readlineSync.question('[+] Masukan Username :');
-var pw = readlineSync.question('[+] Masukan Password :');
-var mail = readlineSync.question('[+] Masukan Email : ')
-const $options = {
-    waitUntil: 'networkidle2'
-	};
-
-
-(async () => {
-    var usgent = randomUseragent.getRandom(function (ua) {
-        return ua.osName === 'Windows';
-	});
-
-  var browser = await puppeteer.launch ({
-    headless: false,
-  });
-  const page = await browser.newPage();
-  await page.goto('https://github.com/join', $options);
-
-  const username = await page.type('#user_login',uname);
-  const email  = await page.type('#user_email',mail);
-  const password = await page.type('#user_password',pw);
-  await page.select('select#all_emails');
-  await page.select('#signup_button');
-
-  try {
-    await page.waitForNavigation($options);
-    console.log('Berhasil Membuat Akun');
-    await browser.close();
-  } catch (error) {
-    console.log('Gagal Membuat Akun');
-    await browser.close();
+class ListNode {
+  constructor(val, next = null) {
+    
+    this.val = val;
+    this.next = next;
   }
-})();
+}
+
+const linkedList = [5, 4, 3, 2, 1].reduce((acc, val) => new ListNode(val, acc), null);
+
+
+const printList = (head) => {
+  if(!head) {
+    return;
+  }
+
+  console.log(head.val);
+  printList(head.next);
+}
+
+// --------- solution -----------
+
+var reverseList = function(head) {
+  let prev = null;
+  let current = head;
+  
+  while(current) {
+    let nextTemp = current.next;
+    current.next = prev;
+    prev = current;
+    current = nextTemp;
+  }
+  
+  return prev;
+};
+
+printList(linkedList);
+console.log('after reverse')
+printList(reverseList(linkedList))
